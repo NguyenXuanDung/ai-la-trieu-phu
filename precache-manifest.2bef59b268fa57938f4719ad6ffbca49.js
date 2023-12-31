ed652e6dce35832267c0ce913436ffd5a7477958
dxnguyen <dung.xuan.nguyen@mgm-tp.com>
2023-12-31 15:20:39 +0700

    Updates

---
+++ precache-manifest.2bef59b268fa57938f4719ad6ffbca49.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "571b548227b5b406e81a",
+    "url": "/ai-la-trieu-phu/static/css/main.2a7dc30a.chunk.css"
+  },
+  {
+    "revision": "571b548227b5b406e81a",
+    "url": "/ai-la-trieu-phu/static/js/main.c9c99d3c.chunk.js"
+  },
+  {
+    "revision": "c53f25d97c4d961a6d6a",
+    "url": "/ai-la-trieu-phu/static/js/runtime~main.8f5c2ad2.js"
+  },
+  {
+    "revision": "cc2655b2970634c6ed52",
+    "url": "/ai-la-trieu-phu/static/js/2.7f3ba32b.chunk.js"
+  },
+  {
+    "revision": "7b0a57fc40d68f68d8aae36d114cafe4",
+    "url": "/ai-la-trieu-phu/static/media/question_1-5.7b0a57fc.mp3"
+  },
+  {
+    "revision": "bfaec84e4d30296703efffb7e454f395",
+    "url": "/ai-la-trieu-phu/static/media/question_6-10.bfaec84e.mp3"
+  },
+  {
+    "revision": "997c15c9b2276b93c796ec5316af46de",
+    "url": "/ai-la-trieu-phu/static/media/question_11-14.997c15c9.mp3"
+  },
+  {
+    "revision": "a15f2b53b2dab210f3d7b1202b396947",
+    "url": "/ai-la-trieu-phu/static/media/question_15.a15f2b53.mp3"
+  },
+  {
+    "revision": "5fdc81ff81936f7b7dd450daa27f7c78",
+    "url": "/ai-la-trieu-phu/static/media/correct_1-4.5fdc81ff.mp3"
+  },
+  {
+    "revision": "cde85b20816b439c1927b9d6201918ef",
+    "url": "/ai-la-trieu-phu/static/media/correct_5.cde85b20.mp3"
+  },
+  {
+    "revision": "c8645b1bf23bc60f0f3a657f8575f4c1",
+    "url": "/ai-la-trieu-phu/static/media/correct_6.c8645b1b.mp3"
+  },
+  {
+    "revision": "5f753a3c3ecde0fb183aa6da4f5d24ee",
+    "url": "/ai-la-trieu-phu/static/media/correct_7.5f753a3c.mp3"
+  },
+  {
+    "revision": "ba4e9b4315f6a432610731e9f6de46e7",
+    "url": "/ai-la-trieu-phu/static/media/correct_8.ba4e9b43.mp3"
+  },
+  {
+    "revision": "64d8c963fe360797b81029cce74eda5e",
+    "url": "/ai-la-trieu-phu/static/media/correct_9.64d8c963.mp3"
+  },
+  {
+    "revision": "0b101cbbbed060a775c3135400bb6903",
+    "url": "/ai-la-trieu-phu/static/media/correct_10.0b101cbb.mp3"
+  },
+  {
+    "revision": "a8228eb70111928c560ff5cc1887a534",
+    "url": "/ai-la-trieu-phu/static/media/correct_11.a8228eb7.mp3"
+  },
+  {
+    "revision": "5b0004e2cf92f7b6f5f35e092c562302",
+    "url": "/ai-la-trieu-phu/static/media/correct_12.5b0004e2.mp3"
+  },
+  {
+    "revision": "24a4aace7dbc590e9d52f40518c86ec1",
+    "url": "/ai-la-trieu-phu/static/media/correct_13.24a4aace.mp3"
+  },
+  {
+    "revision": "37e61acc8367b79bc70fe6451d31a1e6",
+    "url": "/ai-la-trieu-phu/static/media/correct_14.37e61acc.mp3"
+  },
+  {
+    "revision": "e513b6ad986199f9b24c85706115827f",
+    "url": "/ai-la-trieu-phu/static/media/correct_15.e513b6ad.mp3"
+  },
+  {
+    "revision": "76ae575aec391421ba64bf993ec6ffbb",
+    "url": "/ai-la-trieu-phu/static/media/incorrect_1-5.76ae575a.mp3"
+  },
+  {
+    "revision": "e0f55c1058f7a4bae3f2c97add8a8a30",
+    "url": "/ai-la-trieu-phu/static/media/incorrect_6-10.e0f55c10.mp3"
+  },
+  {
+    "revision": "8b3d3ecf21adde450b6f5934acd42332",
+    "url": "/ai-la-trieu-phu/static/media/incorrect_11-14.8b3d3ecf.mp3"
+  },
+  {
+    "revision": "11c0fa5e870729d0072aa4b4ea65c1a5",
+    "url": "/ai-la-trieu-phu/static/media/incorrect_15.11c0fa5e.mp3"
+  },
+  {
+    "revision": "2ea2cf59272adfb9a15e4601dbe011b6",
+    "url": "/ai-la-trieu-phu/static/media/final_1_5.2ea2cf59.mp3"
+  },
+  {
+    "revision": "7a0c6764ab9ed8b7f015273767f46951",
+    "url": "/ai-la-trieu-phu/static/media/final_6.7a0c6764.mp3"
+  },
+  {
+    "revision": "6e341feaf93b901dad42663e8c795721",
+    "url": "/ai-la-trieu-phu/static/media/final_7.6e341fea.mp3"
+  },
+  {
+    "revision": "da4ba231bcf9f67950437d1a88a2a1d8",
+    "url": "/ai-la-trieu-phu/static/media/final_8.da4ba231.mp3"
+  },
+  {
+    "revision": "e36dfa7f706c6f441e7f15a8952ef246",
+    "url": "/ai-la-trieu-phu/static/media/final_9.e36dfa7f.mp3"
+  },
+  {
+    "revision": "00860d9a25c8cbe5bd2c97b39249452b",
+    "url": "/ai-la-trieu-phu/static/media/final_10.00860d9a.mp3"
+  },
+  {
+    "revision": "129b77120851239af1284410f33f71de",
+    "url": "/ai-la-trieu-phu/static/media/final_11.129b7712.mp3"
+  },
+  {
+    "revision": "cd02b8e8ea79ac3f1bf2beec75052e7c",
+    "url": "/ai-la-trieu-phu/static/media/final_12.cd02b8e8.mp3"
+  },
+  {
+    "revision": "d361a903a1528b1f593b4d83b7c09dc2",
+    "url": "/ai-la-trieu-phu/static/media/final_13.d361a903.mp3"
+  },
+  {
+    "revision": "b5415a2ac8e4f6c6b5efdc6af5825792",
+    "url": "/ai-la-trieu-phu/static/media/final_14.b5415a2a.mp3"
+  },
+  {
+    "revision": "d1607314c47fd0c8f75805c49a6ec36e",
+    "url": "/ai-la-trieu-phu/static/media/final_15.d1607314.mp3"
+  },
+  {
+    "revision": "1b9724152538a614ef7eb111f572474d",
+    "url": "/ai-la-trieu-phu/static/media/phone_a_friend_timer.1b972415.mp3"
+  },
+  {
+    "revision": "581d5a40fbadf56b93d152642de90cbe",
+    "url": "/ai-la-trieu-phu/static/media/walk_away.581d5a40.mp3"
+  },
+  {
+    "revision": "599c5056f90af66e3c9a3180fb180495",
+    "url": "/ai-la-trieu-phu/static/media/fifty_fifty.599c5056.mp3"
+  },
+  {
+    "revision": "1e70202f8482623625d01d1d5d2223ab",
+    "url": "/ai-la-trieu-phu/static/media/logo.1e70202f.png"
+  },
+  {
+    "revision": "fb99ec7250d74810d46415371f0ae7e9",
+    "url": "/ai-la-trieu-phu/static/media/intro.fb99ec72.mp4"
+  },
+  {
+    "revision": "d06da0cc99e59f98dc6a356d684e657d",
+    "url": "/ai-la-trieu-phu/static/media/butt_sprite.d06da0cc.png"
+  },
+  {
+    "revision": "d981bfce2b4855eb280a1192f0907eae",
+    "url": "/ai-la-trieu-phu/static/media/sprite_ans.d981bfce.png"
+  },
+  {
+    "revision": "8426d17810efcb596844a9d7932b498e",
+    "url": "/ai-la-trieu-phu/static/media/form_phone.8426d178.png"
+  },
+  {
+    "revision": "fbda7f30620d3460c618fb3a3907d70d",
+    "url": "/ai-la-trieu-phu/index.html"
+  }
+];
